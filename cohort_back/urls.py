c4a536f5c36b7b958c5d8a999d4426222b2b5c75
j.du <julien.dubiel@aphp.fr>
2019-08-20 14:49:01 +0200

    Remove jwt urls

--- cohort_back/urls.py
+++ cohort_back/urls.py
@@ -42,9 +42,9 @@ internal_urls = [
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^jwt/$', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    url(r'^jwt/refresh/$', TokenRefreshView.as_view(), name='token_refresh'),
-    url(r'^jwt/verify/$', TokenVerifyView.as_view(), name='token_verify'),
+    # url(r'^jwt/$', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # url(r'^jwt/refresh/$', TokenRefreshView.as_view(), name='token_refresh'),
+    # url(r'^jwt/verify/$', TokenVerifyView.as_view(), name='token_verify'),
     url(r'^docs/', schema_view),
     url(r'^accounts/', include('rest_framework.urls')),
     url(r'^search/criteria/$', SearchCriteria.as_view(), name="search_criteria"),
