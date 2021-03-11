2e9b78f7e7a3c66643cc92ced84bad44846421e3
Alexandre Martin <alexandre.martin3@aphp.fr>
2021-03-11 14:45:15 +0100

    Fix: forgot a file

--- cohort_back/FhirAPi.py
+++ cohort_back/FhirAPi.py
@@ -5,10 +5,16 @@ class FhirValidateResponse:
 
 
 class FhirCountResponse(FhirValidateResponse):
-    def __init__(self, count: int = None, fhir_datetime=None, fhir_job_id: str = "", job_duration=None,
-                 success: bool = False, err_msg: str = ""):
+    def __init__(self, count: int = None, count_male: int = None, count_unknown: int = None, count_deceased: int = None,
+                 count_alive: int = None, count_female: int = None, fhir_datetime=None, fhir_job_id: str = "",
+                 job_duration=None, success: bool = False, err_msg: str = ""):
         super(FhirCountResponse, self).__init__(success=success, err_msg=err_msg)
         self.count = count
+        self.count_male = count_male
+        self.count_unknown = count_unknown
+        self.count_deceased = count_deceased
+        self.count_alive = count_alive
+        self.count_female = count_female
         self.fhir_datetime = fhir_datetime
         self.job_duration = job_duration
         self.fhir_job_id = fhir_job_id
