a096992db64bd7c9565a86cb7fba018ebeda305a
Alexandre Martin <alexandre.martin3@aphp.fr>
2021-05-19 15:10:28 +0200

    Little bit better documentation

--- cohort_back/urls.py
+++ cohort_back/urls.py
@@ -15,13 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.urls import include, path
-
-from rest_framework import routers
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import routers, permissions
 from rest_framework_swagger.views import get_swagger_view
 
 from cohort.views import UserViewSet
 from explorations.views import SearchCriteria
-
 # Routers provide an easy way of automatically determining the URL conf.
 from voting.views import IssuePost, Thumbs, GitlabIssueViewSet
 
@@ -29,7 +29,18 @@ router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'voting/issues', GitlabIssueViewSet)
 
-schema_view = get_swagger_view(title='Cohort360 API')
+old_schema_view = get_swagger_view(title='Cohort360 API')
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Cohort 360",
+        default_version='v2.3',
+        description="Infos de l'API concernant Cohort 360",
+        terms_of_service="",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 internal_urls = [
 ]
@@ -37,7 +48,9 @@ internal_urls = [
 urlpatterns = [
     url(r'^', include(router.urls)),
     path("explorations/", include(('explorations.urls', 'explorations'), namespace="explorations")),
-    url(r'^docs/', schema_view),
+    url(r'^docs', schema_view.with_ui('swagger', cache_timeout=0, )),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    url(r'^old-docs/', old_schema_view),
     url(r'^accounts/', include('rest_framework.urls')),
     url(r'^search/criteria/$', SearchCriteria.as_view(), name="search_criteria"),
     url(r'^voting/create_issue', IssuePost.as_view(), name='voting_issues'),
