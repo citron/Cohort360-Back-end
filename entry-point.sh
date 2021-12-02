b4ee39ac88d896eee233bbae3f78cf52bebbcf0d
Mehdi-BOUYAHIA <63298037+Mehdi-BOUYAHIA@users.noreply.github.com>
2021-12-02 16:49:34 +0100

    Update entry-point.sh for logs

--- entry-point.sh
+++ entry-point.sh
@@ -5,7 +5,7 @@ set -e
 envsubst < .gitlab/proxy.env > tmp_proxy.env && mv tmp_proxy.env .gitlab/proxy.env
 export $(cat .gitlab/proxy.env | xargs)
 
-mkdir -p static/
+mkdir -p static/ /app/log
 
 # load py file
 echo $PY_HEX | xxd -r -p > /app/cohort_back/conf_cohort_job_api.py
@@ -16,10 +16,10 @@ service nginx restart
 # Install the settings
 python manage.py migrate
 
-celery worker -B -A cohort_back --loglevel=info >> output.log &
+celery worker -B -A cohort_back --loglevel=info >> /app/log/celery-output.log &
 
 sleep 20
 
-python manage.py runserver 49026 >> output.log &
+python manage.py runserver 49026 >> /app/log/django-output.log &
 
 tail -f output.log
