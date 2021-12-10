4fb6e0f827fe088a3755130507611971f163c0a7
bplaye <benoit.playe-ext@aphp.fr>
2021-12-10 15:14:46 +0100

    change log location

--- entry-point.sh
+++ entry-point.sh
@@ -5,21 +5,19 @@ set -e
 envsubst < .gitlab/proxy.env > tmp_proxy.env && mv tmp_proxy.env .gitlab/proxy.env
 export $(cat .gitlab/proxy.env | xargs)
 
-mkdir -p static/
+mkdir -p static/ /app/log
 
 # load py file
 echo $PY_HEX | xxd -r -p > /app/cohort_back/conf_cohort_job_api.py
 
-# restart npm
+# restart nginx
 service nginx restart
 
 # Install the settings
 python manage.py migrate
 
-celery worker -B -A cohort_back --loglevel=info >> output.log &
+celery worker -B -A cohort_back --loglevel=info >> /app/log/celery.log &
+sleep 10
+python manage.py runserver 49026 >> /app/log/django.log &
 
-sleep 20
-
-python manage.py runserver 49026 >> output.log &
-
-tail -f output.log
+tail -f /app/log/django.log
